e3a0b455b7a321e6278327ff407409516eb4e8b3
trentmillar <trent.millar@gmail.com>
2020-01-31 10:41:26 -0700

    output added

--- main.tf
+++ main.tf
@@ -31,3 +31,9 @@ resource "aws_ecr_lifecycle_policy" "this" {
   repository = aws_ecr_repository.this.name
   policy     = file("./lifecycle.json")
 }
+
+locals {
+  output = {
+    aws_ecr_repository_url = aws_ecr_repository.this.repository_url
+  }
+}
